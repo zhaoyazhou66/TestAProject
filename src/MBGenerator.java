72d1b8aae6470b10ceb8c706f432417a67efc794
zhaoyazhou66 <44992798+zhaoyazhou66@users.noreply.github.com>
2018-11-13 13:51:04 +0800

    上传项目文件

---
+++ src/MBGenerator.java
@@ -0,0 +1,43 @@
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+public class MBGenerator {
+
+	/**
+	 * @param args
+	 * @throws XMLParserException
+	 * @throws IOException
+	 * @throws InvalidConfigurationException
+	 * @throws InterruptedException
+	 * @throws SQLException
+	 */
+	public static void main(String[] args) throws IOException,
+			XMLParserException, InvalidConfigurationException, SQLException,
+			InterruptedException {
+		List<String> warnings = new ArrayList<String>();
+		boolean overwrite = true;
+		File directory = new File(".");
+		File configFile = new File(directory.getCanonicalPath()
+				+ File.separator + "src" + File.separator
+				+ "generatorConfig-oracle.xml");
+		ConfigurationParser cp = new ConfigurationParser(warnings);
+		Configuration config = cp.parseConfiguration(configFile);
+		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+		MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,
+				callback, warnings);
+		myBatisGenerator.generate(null);
+		System.out.println("success");
+		
+	}
+
+}
